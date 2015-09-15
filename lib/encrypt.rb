0cf07913b57d2111fbb2778f5b5b06069c330212
acareaga <aaroncareaga@gmail.com>
2015-09-15 08:38:27 -0600

    Started the encrypt class.

---
+++ lib/encrypt.rb
@@ -0,0 +1,27 @@
+require './lib/offset'
+require './lib/key'
+require 'pry'
+
+class Encrypt
+
+  attr_accessor :input_file, :my_output, :text
+
+  def initialize(input_file)
+    @input_file = input_file
+    @text = File.open(input_file).read.split("\n\n")
+  end
+
+  def encrypt_text
+
+  end
+
+  def package_output_file
+    output_file = File.open("encrypted.txt", 'w')
+    # output_file.write(encrypt_text)
+    puts "Created '#{output_file}' with the key #{key} and date #{date}"
+ end
+
+end
+
+input_file = ARGV[0]
+Encrypt.new(input_file).package_output_file
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -3,10 +3,17 @@
 # generate key
 # generate offset
 # pass key and offset into encryptor
-# rotate on codemap for plain text character using the key + offset
+# rotate on charactermap for plain text character using the key + offset
+
+# Four characters are encrypted at a time.
+# The first character is rotated forward by the "A" rotation plus the "A offset"
+# The second character is rotated forward by the "B" rotation plus the "B offset"
+# The third character is rotated forward by the "C" rotation plus the "C offset"
+# The fourth character is rotated forward by the "D" rotation plus the "D offset"
+
 # save encrypted letter in encrypted array
 # repeat process until ..end.. is reached
-# write output file with encrpyed array
+# write output file (encrypted.txt) with encrpyed array
 # display: Created 'encrypted.txt' with the key 82648 and date 030415
 
 # repeat process with decrypt and crack.rb
