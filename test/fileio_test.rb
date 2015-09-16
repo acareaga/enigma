9ab0ad542c4d797e507806a18e3ce156275f3319
acareaga <aaroncareaga@gmail.com>
2015-09-16 15:11:07 -0600

    Tested FileIO input and standardization of text. 2 tests passing.

--- test/fileio_test.rb
+++ test/fileio_test.rb
@@ -2,18 +2,26 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/fileio'
+require 'pry'
 
 class FileIOTest < Minitest::Test
 
-  def test_fileio_exists
+  def test_fileio_can_open_input
     input_file = '../message.txt'
     assert FileIO.new(input_file)
   end
 
-  def test_fileio_opens_input
+  def test_fileio_removes_newline_characters
     input_file = '../message.txt'
-    text = File.open(input_file).read
-    assert_equal text, input_file
+    refute_equal FileIO.new(input_file), File.open(input_file).read
+  end
+
+  def test_fileio_packages_output_file
+    skip
+    encrypt_text = # NEED TO ADD CONVERTER STUFF
+    output_file = File.open("encrypted.txt", 'w')
+    output_file.write(encrypt_text)
+    assert_equal encrypted_text, output_file
   end
 
 end
