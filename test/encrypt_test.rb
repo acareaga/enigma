314adfab3cee908181561dca420c914793bf09a5
acareaga <aaroncareaga@gmail.com>
2015-09-16 15:38:15 -0600

    Text passes into encrypt. Working on tests for converter.

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,4 +1,4 @@
-gem 'minitest'
+require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/encrypt'
@@ -6,70 +6,72 @@ require '../lib/encrypt'
 class EncryptTest < Minitest::Test
 
   def test_encrypt_class_exists
-    data = Encrypt.new("hello")
-
+    data = Encrypt.new
     assert_equal Encrypt, data.class
   end
 
-  def test_it_can_take_in_a_file
-
+  def test_input_file_returns_text_from_fileio
+    skip
+    input_file = './message.txt'
+    text = FileIO.new(input_file).file
+    assert text
   end
 
-  def test_input_file_converts_text_to_string_omitting_new_lines
-
+  def test_split_text_converts_to_chunks
+    skip
   end
 
   def test_it_rejects_files_with_invalid_characters
-
+    skip
   end
 
   def test_text_splits_into_chunks_of_four_letter_long_strings
-
+    skip
   end
 
   def test_each_string_is_split_into_individual_characters
-
+    skip
   end
 
   def test_character_map_exists
+    skip
     test = Encrypt.new("hello")
-
     assert test.character_map
   end
 
   def test_character_map_can_access_the_first_character
+    skip
     test = Encrypt.new("hello")
-
     assert_equal "a", test.character_map[0]
   end
 
   def test_character_map_can_access_the_last_character
+    skip
     test = Encrypt.new("hello")
-
     assert_equal ",", test.character_map[-1]
   end
 
   def test_character_map_can_access_a_character_in_the_middle
+    skip
     test = Encrypt.new("hello")
-
     assert_equal "p", test.character_map[15]
     assert_equal 15, test.character_map.index("p")
   end
 
   def test_character_map_does_not_include_invalid_characters
+    skip
     test = Encrypt.new("hello")
-
     refute test.character_map.include?("?")
   end
 
   def test_we_can_navigate_the_character_map_forward
+    skip
     test = Encrypt.new("hello")
-
     assert_equal
   end
 
   def test_we_can_navigate_the_character_map_backwards
-
+    skip
   end
 
 end
