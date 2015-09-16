ca1daa373f85170fd8c1a6c41f40d7ca95e667f2
acareaga <aaroncareaga@gmail.com>
2015-09-16 08:32:23 -0600

    Fixed the key offset adding.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -23,23 +23,24 @@ class Encrypt
     combine_offset_and_key_abcd
   end
 
-  def character_map
-    character_map = ('a'..'z').to_a + ('0'..'9').to_a + [" ", ".", ","]
-  end
-
   def combine_offset_and_key_abcd
     offset = Offset.new
     key = Key.new
-    rotate = offset.position + key.position
+    a = offset.position[0] + key.position[0].to_i
+    b = offset.position[1] + key.position[1].to_i
+    c = offset.position[2] + key.position[2].to_i
+    d = offset.position[3] + key.position[3].to_i
+    rotate = [a, b, c, d]
     binding.pry
   end
 
-  # def encrypt_text
-  #   characters.each do |nested_array|
-  #     encrypt_text << nested_array.rotate
-  #   end
-  # end
-  #
+  def encrypt_text
+    character_map = ('a'..'z').to_a + ('0'..'9').to_a + [" ", ".", ","]
+    characters.each do |nested_array|
+      encrypt_text << nested_array.rotate
+    end
+  end
+
   # def rotate
   #   nested_array.each do |character|
   #     encrypted_text << # rotate through map
@@ -49,7 +50,7 @@ class Encrypt
 end
 
 input_file = ARGV[0]
-FileIO.new(input_file).package_output_file
+Encrypt.new(input_file).package_output_file
 
 if im_running_code
   input_file = ARGV[0]
